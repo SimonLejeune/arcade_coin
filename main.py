99db6a05c8d858e6529854ad190a4d6acf13dd59
Simon Lejeune <simon.lejeune@epitech.eu>
2019-01-26 15:41:02 +0100

    first python file that wait for rfid and read uid

---
+++ main.py
@@ -0,0 +1,36 @@
+from smartcard.CardMonitoring import CardMonitor, CardObserver
+from smartcard.scard import *
+from smartcard.util import *
+import time
+
+class printobserver( CardObserver ):
+    def update( self, observable, (addedcards, removedcards) ):
+        for card in addedcards:
+         if addedcards:
+            hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+            assert hresult==SCARD_S_SUCCESS
+            hresult, readers = SCardListReaders(hcontext, [])
+            assert len(readers)>0
+            reader = readers[0]
+            hresult, hcard, dwActiveProtocol = SCardConnect(
+             hcontext,
+             reader,
+             SCARD_SHARE_SHARED,
+             SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1)
+            hresult, response = SCardTransmit(hcard,dwActiveProtocol,[0xFF,0xCA,0x00,0x00,0x00])
+            uid = toHexString(response, format=0)
+            print("card atr : ", response)  # cards ATR
+            print("cart uid : ", uid)  # Cards UID
+            id = uid.replace(" ", "")
+            print("id : ", id)
+            id = id[:-4]
+            print("new id : ", id)
+
+
+print("place card on reader")
+while 1:
+    cardmonitor = CardMonitor()
+    cardobserver = printobserver()
+    cardmonitor.addObserver( cardobserver )
+    cardmonitor.deleteObserver( cardobserver )
+    time.sleep( 2 )
