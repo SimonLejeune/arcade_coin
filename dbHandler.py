6836841450c4cb4cc98be7873ce0e338c66e2f28
Simon Lejeune <simon.lejeune@epitech.eu>
2019-01-26 19:37:52 +0100

    trying to create own function

--- dbHandler.py
+++ dbHandler.py
@@ -13,37 +13,27 @@ class Connection:
                     'CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, uid TEXT UNIQUE, credit INTEGER, email TEXT, creation_date DATETIME, last_played DATETIME);')
             except Error as e:
                 print(e)
-        self.conn.row_factory = sqlite3.Row
-        self.c = self.conn.cursor()
-        self.insertNext = True
 
     def insertNew(self, uid, credit=1, email=""):
         self.c.execute(
             'INSERT OR IGNORE INTO users(uid, credit, email, creation_date) VALUES( {}, {}, {}, "{}", datetime("now"))'.format(
                 uid, credit, email))
         self.conn.commit()
-        self.insertNext = False
         return 'created'
 
-    def insertTimeOut(self):
-        self.insertNext = False
 
-    def removeCredit(self, uid, email):
-        if self.insertNext:
-            return self.insertNew(uid, email)
+    def removeCredit(self, uid):
         self.c.execute('SELECT * FROM users WHERE uid=' + str(uid))
         user = self.c.fetchone()
         if user == None:
             return 'reject'
-        type = user[2]
         credits = user[3]
-        if user is None or credits < 1 and type > 0:
+        if user is None or credits < 1:
             return 'reject'
-
         self.c.execute('UPDATE users SET credit=' + str(credits - 1) + ' WHERE uid=' + str(uid))
         self.conn.commit()
         return "accept"
 
-    def addAllCredit(self, credit=10):
+    def addAllCredit(self, credit=1):
         self.c.execute('update users SET credit={} where credit<{}'.format(credit, credit))
         self.conn.commit()
