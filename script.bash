cec60f6b04efca478cc1fafb28ebcdad856d9c57
Simon Lejeune <simon.lejeune@epitech.eu>
2019-03-26 12:20:02 +0100

    adding script that contains command and update for readme

---
+++ script.bash
@@ -0,0 +1,11 @@
+sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+
+https://oneguyoneblog.com/2015/09/16/acr122u-nfc-usb-reader-raspberry-pi/
+
+@reboot sudo /usr/bin/node /home/pi/arcade_coin/app.js &
+
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
